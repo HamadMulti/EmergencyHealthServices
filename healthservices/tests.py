995d3ce20e71bf1898dfd3f6b6266dbebffe3126
HamadMulti <50540486+HamadMulti@users.noreply.github.com>
2025-11-05 16:36:43 +0000

    Update tests.py

--- healthservices/tests.py
+++ healthservices/tests.py
@@ -1,14 +1,7 @@
-from django.test import TestCase
 
-# Create your tests here.
-class BasicTest(TestCase):
-    def test_basic_math(self):
-        """Test basic arithmetic"""
-        self.assertEqual(2 + 3, 5)
-        self.assertEqual(-1 + 1, 0)
-        self.assertNotEqual(2 + 2, 5)
     
     def test_homepage_exists(self):
         """Test that signin page loads"""
         response = self.client.get('/')
+
         self.assertEqual(response.status_code, 200)
